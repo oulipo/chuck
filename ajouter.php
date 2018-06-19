6eb4e55493bb3b7d6d24b8aa4af92df10a6dbeae
oulipo <laudesj@gmail.com>
2018-06-19 16:45:46 +0200

    heroku test

--- ajouter.php
+++ ajouter.php
@@ -6,11 +6,27 @@ $texte = $_POST["texte"] ?? "";
 if(!empty($texte)) {
     $stmt = $pdo->prepare("INSERT INTO citations (texte, date_insertion) VALUES (?,?)");
     $result = $stmt->execute([$texte, (new DateTime("now"))->format("Y-m-d")]);
+    $id = $pdo->lastInsertId();
     if($result) {
-        echo "OK";
+        //echo "OK";
+        $resultat = [
+            "id" => $id,
+            "status" => "OK"
+        ];
+        echo json_encode($resultat);
     } else {
-        echo "NOK";
+        //echo "NOK";
+        $resultat = [
+            "id" => 0,
+            "status" => "NOK"
+        ];
+        echo json_encode($resultat);
     }
 } else {
-    echo "NOK";
+    //echo "NOK";
+    $resultat = [
+        "id" => 0,
+        "status" => "NOK"
+    ];
+    echo json_encode($resultat);
 }
